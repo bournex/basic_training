cfcdeda65fedf5971244f2c8b12dac500dd1efce
liuxuefei <liuxuefei@qxlive.cn>
2021-03-29 15:50:19 +0800

    fix

---
+++ leetcode/question15/three_sum.go
@@ -0,0 +1,40 @@
+package question15
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	if len(nums) < 3 {
+		return nil
+	}
+	sort.Ints(nums)
+	res := make([][]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		j := i + 1         // 正向索引
+		k := len(nums) - 1 // 反向索引
+
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		for j < len(nums) && j != k {
+			if j > i+1 && nums[j] == nums[j-1] {
+				j++
+				continue
+			}
+
+			if nums[i]+nums[j]+nums[k] < 0 {
+				j++
+				continue
+			} else if nums[i]+nums[j]+nums[k] > 0 {
+				k--
+			} else if nums[i]+nums[j]+nums[k] == 0 {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				j++
+			}
+
+		}
+	}
+
+	return res
+}
---
+++ leetcode/question15/three_sum_test.go
@@ -0,0 +1,19 @@
+package question15
+
+import "testing"
+
+func TestThreeSum(t *testing.T) {
+	// input := [][]int{{-1, 0, 1, 2, -1, -4}, {}, {0}, {0, 0, 0}, {0, 0, 0, 0}, {-1, 0, 1, 2, -1, -4}}
+	// expect := [][][]int{{{-1, -1, 2}, {-1, 0, 1}}, {}, {}, {{0, 0, 0}}, {{0, 0, 0}}, {{-1, -1, 2}, {-1, 0, 1}}}
+	input := [][]int{{-1, 0, 1, 2, -1, -4}}
+	expect := [][][]int{{{-1, -1, 2}, {-1, 0, 1}}}
+
+	for i, v := range input {
+		res := threeSum(v)
+		e := expect[i]
+		if len(res) != len(e) {
+			t.Errorf("input %+v, expect %+v, got %+v", v, e, res)
+		}
+		t.Logf("expect %+v, got %+v", e, res)
+	}
+}
