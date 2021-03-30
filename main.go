276c4046dcc1ef4edd1c538cab3f195a5f1bfb28
liuxuefei <liuxuefei@qxlive.cn>
2021-03-30 13:31:58 +0800

    fix

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 
-	"github.com/bournex/basic_training/algorithms"
 	"github.com/bournex/basic_training/interview"
 	"github.com/bournex/basic_training/structures"
 	"github.com/bournex/basic_training/working"
@@ -13,7 +12,7 @@ func main() {
 	print := flag.Bool("p", false, "print detail")
 	flag.Parse()
 
-	algorithms.AlgorithmEntry(*print)
+	// algorithms.AlgorithmEntry(*print)
 	structures.StructureEntry(*print)
 	interview.InterviewEntry(*print)
 	working.WorkingEntry(*print)
