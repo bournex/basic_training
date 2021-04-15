a955036e42e8716ab96bdcf75f638decc64c67d3
liuxuefei <liuxuefei@qxlive.cn>
2021-04-15 13:55:39 +0800

    fix

--- lc/array/q163/find_missing_ranges.go
+++ lc/array/q163/find_missing_ranges.go
@@ -1,10 +1,49 @@
 package q163
 
+import "fmt"
+
 //  TODO
 func findMissingRanges(nums []int, lower int, upper int) []string {
+	if len(nums) == 0 {
+		if lower == upper {
+			return []string{fmt.Sprintf("%d", lower)}
+		} else {
+			return []string{fmt.Sprintf("%d->%d", lower, upper)}
+		}
+	}
+	result := make([]string, 0)
+	for i := 1; i < len(nums); i++ {
+		n := nums[i] - nums[i-1]
+
+		if n == 2 {
+			result = append(result, fmt.Sprintf("%d", nums[i-1]+1))
+		} else if n > 2 {
+			result = append(result, fmt.Sprintf("%d->%d", nums[i-1]+1, nums[i]-1))
+		}
+	}
+
+	start, end := "", ""
+	if nums[0]-lower == 1 {
+		start = fmt.Sprintf("%d", lower)
+	} else if nums[0]-lower > 1 {
+		start = fmt.Sprintf("%d->%d", lower, nums[0]-1)
+	}
+
+	if upper-nums[len(nums)-1] == 1 {
+		end = fmt.Sprintf("%d", upper)
+	} else if upper-nums[len(nums)-1] > 1 {
+		end = fmt.Sprintf("%d->%d", nums[len(nums)-1]+1, upper)
+	}
+
+	if start != "" {
+		result = append([]string{start}, result[:]...)
+	}
+	if end != "" {
+		result = append(result, end)
+	}
 
 	// 处理结尾
-	return nil
+	return result
 }
 
 // 思考
