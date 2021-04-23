f35bd7413ec42ae116ebc051c05c3402f83d0c95
Liuyc <369120032@qq.com>
2021-04-23 12:55:52 +0800

    改为纯查表

--- lc/string/m12/int_to_roman.go
+++ lc/string/m12/int_to_roman.go
@@ -9,21 +9,51 @@ package m12
 // D             500
 // M             1000
 
-// 原则：同一个阿拉伯数字有多种罗马数字表达，尽量选择大的罗马数字
 // 贪婪查表
+//	难点在于，是否能想到，同一个阿拉伯数字有多种罗马数字表达
+//	这是采用贪婪算法的关键
 func intToRoman(num int) string {
-	arabic := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	roman := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
-
 	var result string
-	n := len(arabic)
 	for num > 0 {
-		for i := 0; i < n; i++ {
-			if num >= arabic[i] {
-				num -= arabic[i]
-				result += roman[i]
-				break
-			}
+		if num >= 1000 {
+			num -= 1000
+			result += "M"
+		} else if num >= 900 {
+			num -= 900
+			result += "CM"
+		} else if num >= 500 {
+			num -= 500
+			result += "D"
+		} else if num >= 400 {
+			num -= 400
+			result += "CD"
+		} else if num >= 100 {
+			num -= 100
+			result += "C"
+		} else if num >= 90 {
+			num -= 90
+			result += "XC"
+		} else if num >= 50 {
+			num -= 50
+			result += "L"
+		} else if num >= 40 {
+			num -= 40
+			result += "XL"
+		} else if num >= 10 {
+			num -= 10
+			result += "X"
+		} else if num >= 9 {
+			num -= 9
+			result += "IX"
+		} else if num >= 5 {
+			num -= 5
+			result += "V"
+		} else if num >= 4 {
+			num -= 4
+			result += "IV"
+		} else if num >= 1 {
+			num -= 1
+			result += "I"
 		}
 	}
 
