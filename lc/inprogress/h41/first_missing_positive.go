b8457d924f7d0b6492ffa991a2f181f5a682d64c
Liuyc <369120032@qq.com>
2021-05-25 13:40:21 +0800

    fix

--- lc/inprogress/h41/first_missing_positive.go
+++ lc/inprogress/h41/first_missing_positive.go
@@ -5,7 +5,22 @@ package h41
 //	基于这条原则，只需要遍历两遍数组即可，第一遍从0到len(nums)-1，负数保持为负数，大于len(nums)的数置为-1，如果遇到大于等于1，小于等于len(nums)，则将其放置到其值-1对应的slot上
 //	如果该位置也有大于等于1，小于等于len(nums)的数字，则循环做同样的事情，直到所有值遍历完
 //	第二遍只要从头找第一个为-1的位子，其索引+1就是目标值
-func firstMissingPositive(nums []int) int {
 
-	return 0
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		idx := nums[i]
+		nums[i] = -1
+		for idx > 0 && idx <= n && idx != nums[idx-1] {
+			idx, nums[idx-1] = nums[idx-1], idx
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if nums[i] == -1 {
+			return i + 1
+		}
+	}
+
+	return n + 1
 }
--- lc/inprogress/h41/first_missing_positive_test.go
+++ lc/inprogress/h41/first_missing_positive_test.go
@@ -5,12 +5,35 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestFirstMissingPositive(t *testing.T) {
 	testCases := []struct {
 		Input  []int
 		Expect int
 	}{
-		{},
+		{
+			[]int{1, 2, 0},
+			3,
+		},
+		{
+			[]int{3, 7, 6, 4, 1, 5, 0, 9, 2},
+			8,
+		},
+		{
+			[]int{1, 2, 3},
+			4,
+		},
+		{
+			[]int{100, 101, 102},
+			1,
+		},
+		{
+			[]int{3, 4, -1, 1},
+			2,
+		},
+		{
+			[]int{7, 8, 9, 11, 12},
+			1,
+		},
 	}
 	for i, v := range testCases {
 		t.Run(fmt.Sprintf("case %d", i+1), func(t *testing.T) {
